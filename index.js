2d23bc261cac81bb23c6345ae4b79e7a530a7d46
silvestrevivo <silvestrevivomillan@gmail.com>
2018-06-19 23:30:19 +0200

    axios added for fetching data

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-import str from './search'
+//import str from './search'
+import axios from 'axios'
+import keyapi from './keyapi'
 
-console.log(str)
+
+async function getResults(query) {
+  const PROXY = 'https://cors-anywhere.herokuapp.com/';
+  const result = await axios(`${PROXY}http://food2fork.com/api/search?key=${keyapi}&q=${query}`)
+
+  console.log(result.data.recipes)
+}
+
+getResults();
+
+
