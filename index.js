2de57c3566e72dc5e1e8f048c2db07bab5364cf6
silvestrevivo <silvestrevivomillan@gmail.com>
2018-06-20 00:02:16 +0200

    basic funtionality for fetching data by class

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-//import str from './search'
-import axios from 'axios'
-import keyapi from './keyapi'
+import Search from './search'
 
+const search = new Search('pizza');
 
-async function getResults(query) {
-  const PROXY = 'https://cors-anywhere.herokuapp.com/';
-  const result = await axios(`${PROXY}http://food2fork.com/api/search?key=${keyapi}&q=${query}`)
+console.log(search)
+search.getResults(); // this method creates a new element
 
-  console.log(result.data.recipes)
-}
-
-getResults();
-
-
+setTimeout(() => {
+  console.log(search)
+}, 2000);
--- search.js
+++ search.js
@@ -1,1 +1,19 @@
-export default 'this is a string';
+import axios from 'axios'
+import keyapi from './keyapi'
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const PROXY = 'https://cors-anywhere.herokuapp.com/';
+    try {
+      const res = await axios(`${PROXY}http://food2fork.com/api/search?key=${keyapi}&q=${this.query}`)
+      this.result = res.data.recipes;
+      console.log(this.result)
+    } catch (error) {
+      alert(error)
+    }
+  }
+}
