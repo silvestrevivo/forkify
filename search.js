627e48af3a5b6a6ea6b66639d861f8c15fe15094
silvestrevivo <silvestrevivomillan@gmail.com>
2018-06-21 23:39:06 +0200

    object global state creted asynchronously

--- search.js
+++ search.js
@@ -11,7 +11,7 @@ export default class Search {
     try {
       const res = await axios(`${PROXY}http://food2fork.com/api/search?key=${keyapi}&q=${this.query}`)
       this.result = res.data.recipes;
-      console.log(this.result)
+      // console.log(this.result)
     } catch (error) {
       alert(error)
     }
